2d6e86b4b39d9ca7b4498c567e6ec5226484ca80
安部大貴 <76642241+NTD1995@users.noreply.github.com>
2023-12-12 21:02:19 +0900

    表示させる

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,21 @@ type Todo = {
 };
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  // const [todos, setTodos] = useState<Todo[]>([]);
+
+  const todos: Todo;
+  [] = [
+    {
+      id: 1,
+      task: "掃除",
+      completed: false,
+    },
+    {
+      id: 2,
+      task: "洗濯",
+      completed: true,
+    },
+  ];
 
   return (
     <div className="App">
