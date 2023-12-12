fc4e63f7cf27dbf89059e28b2c22a44f7bed380c
安部大貴 <76642241+NTD1995@users.noreply.github.com>
2023-12-12 20:12:40 +0900

    first commi

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./css/App.css";
+import AddTodo from "./components/AddTodo";
+import TodoList from "./components/TodoList";
+import DeleteTodo from "./components/DeleteTodo";
+
+type Todo = {
+  id: number;
+  task: string;
+  completed: boolean;
+};
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>- Todoアプリ -</h1>
+      <AddTodo todos={todos} setTodos={setTodos} />
+      <TodoList todos={todos} setTodos={setTodos} />
+      <p>残りTodo件数 : {todos.length}件</p>
+      <DeleteTodo todos={todos} setTodos={setTodos} />
     </div>
   );
 }
