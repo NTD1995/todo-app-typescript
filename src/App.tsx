bf70e2d1edfc3907c587979caf3db7ca417b6c54
安部大貴 <76642241+NTD1995@users.noreply.github.com>
2024-03-14 22:11:04 +0900

    my-app修正

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,28 @@ import "./App.css";
 type Todo = {
   id: number;
   title: string;
+  status: string;
+  detail: string;
 };
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([
-    { id: 1, title: "テスト" },
-    { id: 1, title: "テスト" },
-    { id: 1, title: "テスト" },
+    { id: 1, title: "テスト", status: "未着手", detail: "あ" },
+    { id: 1, title: "テスト", status: "未着手", detail: "あ" },
+    { id: 1, title: "テスト", status: "未着手", detail: "あ" },
   ]);
   const [todoTitle, setTodoTitle] = useState("");
   const [todoId, setTodoId] = useState(todos.length + 1);
+  const [todoStatus, setTodoStatus] = useState("");
+  const [todoDetail, setTodoDetail] = useState("");
   const handleAddFormChanges = (e: any) => {
     setTodoTitle(e.target.value);
   };
   const handleAddTodo = () => {
-    setTodos([...todos, { title: todoTitle, id: todoId }]);
+    setTodos([
+      ...todos,
+      { id: todoId, title: todoTitle, status: todoStatus, detail: todoDetail },
+    ]);
 
     setTodoId(todoId + 1);
     setTodoId(todoId + 1);
@@ -38,7 +45,9 @@ function App() {
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>
-            <span>{todo.title}</span>
+            <span>
+              {todo.title} {todo.status} {todo.detail}
+            </span>
             <button onClick={() => handleDeleteTodo(todo)}>削除</button>
           </li>
         ))}
