39f4cadaec73610e624aa6c19a101f312294f0ba
Shogo Wada <aiz4338150@gmail.com>
2024-04-11 21:30:38 +0900

    更新する処理の説明追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
 
 type Todo = {
   id: number;
@@ -10,31 +10,35 @@ type Todo = {
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([
-    { id: 1, title: "テスト", status: "未着手", detail: "あ" },
-    { id: 1, title: "テスト", status: "未着手", detail: "あ" },
-    { id: 1, title: "テスト", status: "未着手", detail: "あ" },
+    { id: 1, title: 'テスト', status: '未着手', detail: 'あ' },
+    { id: 2, title: 'テスト', status: '未着手', detail: 'あ' },
+    { id: 3, title: 'テスト', status: '未着手', detail: 'あ' },
   ]);
-  const [todoTitle, setTodoTitle] = useState("");
+  const [todoTitle, setTodoTitle] = useState('');
   const [todoId, setTodoId] = useState(todos.length + 1);
-  const [todoStatus, setTodoStatus] = useState("");
-  const [todoDetail, setTodoDetail] = useState("");
+  const [todoStatus, setTodoStatus] = useState('');
+  const [todoDetail, setTodoDetail] = useState('');
 
-  const handleAddFormChanges = (e: any) => {
-    setTodoTitle(e.target.value);
-  };
+  // 入力された値でtitle（todoTitle）を更新する処理
+  // eはイベントオブジェクト（eventのe）
+  // イベントとは、ユーザーが行う操作（クリック、入力、スクロールなど）のこと
+  // イベントオブジェクトはイベントが発生した時の情報が入っている
+  // e.target.valueはイベントが発生した要素のvalue（入力された値）を取得する
+
+  // todoを追加する処理
   const handleAddTodo = () => {
-    setTodos([
-      ...todos,
-      { id: todoId, title: todoTitle, status: todoStatus, detail: todoDetail },
-    ]);
+    setTodos([...todos, { id: todoId, title: todoTitle, status: todoStatus, detail: todoDetail }]);
 
     setTodoId(todoId + 1);
     setTodoId(todoId + 1);
-    setTodoTitle("");
+    setTodoTitle('');
   };
+
+  // todoを削除する処理
   const handleDeleteTodo = (targetTodo: any) => {
     setTodos(todos.filter((todo: any) => todo !== targetTodo));
   };
+
   // 入力された値でdetail（todoDetail）を更新する処理
 
   // 選択された値でstatus（todoStatus）を更新する処理
@@ -43,27 +47,22 @@ function App() {
     <>
       <div>
         {/* titleの入力フォーム */}
-        <label style={{ display: "block" }} htmlFor="title">
+        <label style={{ display: 'block' }} htmlFor="title">
           タイトル
         </label>
         <textarea
-          style={{ width: "20em", border: "1px solid #333" }}
+          style={{ width: '20em', border: '1px solid #333' }}
           id="title"
           name="title"
           value={todoTitle}
-          onChange={(e) => setTodoTitle(e.target.value)}
+          // 本来は引数を受け取る場合は () => {} の形でないといけないが、onChangeは指定された関数に自動でイベントオブジェクトを渡すため、
+          // イベントオブジェクトだけを受け取る場合は関数名のみでOK
         />
         {/* detailの入力フォーム */}
-        <label style={{ display: "block" }} htmlFor="title">
+        <label style={{ display: 'block' }} htmlFor="detail">
           詳細
         </label>
-        <textarea
-          style={{ width: "40em", border: "1px solid #333" }}
-          id="title"
-          name="title"
-          value={todoTitle}
-          onChange={(e) => setTodoTitle(e.target.value)}
-        />
+        <textarea style={{ width: '40em', border: '1px solid #333' }} id="detail" name="detail" value={todoDetail} />
         {/* statusを選択するためのプルダウン */}
         <select value={todoStatus}>
           <option value="notStarted">未着手</option>
