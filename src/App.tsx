325fb543306b8c4c40790d2939ffb642d44c4dc1
Shogo Wada <aiz4338150@gmail.com>
2024-05-10 21:48:38 +0900

    課題2まで完了！

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
 
 type Todo = {
   id: number;
@@ -10,30 +10,30 @@ type Todo = {
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([
-    { id: 1, title: "テスト", status: "未着手", detail: "あ" },
-    { id: 2, title: "テスト", status: "未着手", detail: "あ" },
-    { id: 3, title: "テスト", status: "未着手", detail: "あ" },
+    { id: 1, title: 'テスト', status: '未着手', detail: 'あ' },
+    { id: 2, title: 'テスト', status: '未着手', detail: 'あ' },
+    { id: 3, title: 'テスト', status: '未着手', detail: 'あ' },
   ]);
-  const [todoTitle, setTodoTitle] = useState("");
+  const [todoTitle, setTodoTitle] = useState('');
   const [todoId, setTodoId] = useState(todos.length + 1);
-  const [todoStatus, setTodoStatus] = useState("");
-  const [todoDetail, setTodoDetail] = useState("");
+  const [todoStatus, setTodoStatus] = useState('');
+  const [todoDetail, setTodoDetail] = useState('');
   const [isEditable, setIsEditable] = useState(false);
   // 課題4
   // 編集対象のtodoのidを保持するstate（editId）を作成する
   // todoのidは数値なので、型はnumberにする
   // この下の定義を修正してください
-  const [editId, setEditId] = useState("");
+  const [editId, setEditId] = useState('');
   // 編集用のタイトルの入力フォームの入力値を保持するstate（newTitle）
-  const [newTitle, setNewTitle] = useState("");
+  const [newTitle, setNewTitle] = useState('');
   // 課題3
   // 編集用の詳細の入力フォームの入力値を保持するstate（newDetail）
   // この下の定義を修正してください
-  const [newDetail, setNewDetail] = useState("");
+  const [newDetail, setNewDetail] = useState('');
   // 課題3
   // 編集用のステータスのプルダウンの入力値を保持するstate（newStatus）
   // この下の定義を修正してください
-  const [newStatus, setNewStatus] = useState("");
+  const [newStatus, setNewStatus] = useState('');
 
   // eはイベントオブジェクト（eventのe）
   // イベントとは、ユーザーが行う操作（クリック、入力、スクロールなど）のこと
@@ -49,16 +49,13 @@ function App() {
   const handleAddTodo = () => {
     // バリデーション
     // todoTitleが空の場合は何もしない
-    if (todoTitle === "") return;
+    if (todoTitle === '') return;
 
-    setTodos([
-      ...todos,
-      { id: todoId, title: todoTitle, status: todoStatus, detail: todoDetail },
-    ]);
+    setTodos([...todos, { id: todoId, title: todoTitle, status: todoStatus, detail: todoDetail }]);
 
     setTodoId(todoId + 1);
     setTodoId(todoId + 1);
-    setTodoTitle("");
+    setTodoTitle('');
   };
 
   // todoを削除する処理
@@ -94,7 +91,7 @@ function App() {
   };
 
   // 編集用のタイトルの入力フォームの入力値を更新する処理
-  const handleEditFormChange = (e: any) => {
+  const handleEditTitleChange = (e: any) => {
     setNewTitle(e.target.value);
   };
 
@@ -111,7 +108,7 @@ function App() {
     // 編集フォームを閉じる
     setIsEditable(false);
     // 編集対象のtodoのidをリセットする
-    setEditId("");
+    setEditId('');
     // 課題5
     // 編集用のタイトルの入力フォームをリセットする
     // この下に処理を記述してください
@@ -131,16 +128,14 @@ function App() {
     // 編集対象のtodoを入力されたtitle、detail、statusで更新した新しい配列（newArray）を作成する
     // mapメソッドを使って、編集対象のtodoのid（editId）とidが一致するtodoのtitle、detail、statusを更新する
     // この下の処理を修正してください
-    const newArray = todos.map((todo) =>
-      todo.id === editId ? { ...todo, title: newTitle } : todo
-    );
+    // const newArray = todos.map((todo) => (todo.id === editId ? { ...todo, title: newTitle } : todo));
     // todosをnewArrayで更新する
-    setTodos(newArray);
+    // setTodos(newArray);
 
     // 編集対象のtodoのidをリセットする
-    setEditId("");
+    setEditId('');
     // 入力されたタイトルをリセットする
-    setNewTitle("");
+    setNewTitle('');
     // 課題5
     // 入力された詳細をリセットする
     // この下に処理を記述してください
@@ -152,7 +147,7 @@ function App() {
     // 課題4
     // 編集フォームを閉じる
     // この下の処理を修正してください
-    handleCloseEditForm("");
+    // handleCloseEditForm('');
   };
 
   return (
@@ -160,11 +155,11 @@ function App() {
       {/* todoを新規追加するためのフォーム */}
       <div>
         {/* タイトルの入力フォーム */}
-        <label style={{ display: "block" }} htmlFor="title">
+        <label style={{ display: 'block' }} htmlFor="title">
           タイトル
         </label>
         <textarea
-          style={{ width: "20em", border: "1px solid #333" }}
+          style={{ width: '20em', border: '1px solid #333' }}
           id="title"
           name="title"
           value={todoTitle}
@@ -173,11 +168,11 @@ function App() {
         <button onClick={handleEditTodo}>編集を保存</button>
         <button onClick={handleCloseEditForm}>キャンセル</button>
         {/* 詳細の入力フォーム */}
-        <label style={{ display: "block" }} htmlFor="detail">
+        <label style={{ display: 'block' }} htmlFor="detail">
           詳細
         </label>
         <textarea
-          style={{ width: "40em", border: "1px solid #333" }}
+          style={{ width: '40em', border: '1px solid #333' }}
           id="detail"
           name="detail"
           value={todoDetail}
@@ -211,63 +206,61 @@ function App() {
             {/* 課題2 */}
             {/* 編集ボタンを押すとtodoを編集するためのフォームが開くようにする */}
             {/* 現在は編集ボタンを押すとtodoが削除されてしまうので、修正してください */}
-            <button onClick={() => handleOpenEditTodo(todo)}>編集</button>
+            <button onClick={() => handleOpenEditForm(todo)}>編集</button>
             <button onClick={() => handleDeleteTodo(todo)}>削除</button>
           </li>
         ))}
       </ul>
       {/* todoを編集するためのフォーム */}
       {/* 課題2 */}
-      {/* 「編集」ボタンを押したら開くようにする */}
+      {/* todoを編集するためのフォームの表示を切り替えることができように設定する */}
       {/* この下のdivタグが編集ボタンを押すことで現れるようにしてください */}
-      const handleOpenEditForm = () => {
-    setIsEditable(true)
-  }
       {/* 課題3 */}
       {/* 作成したフォームやプルダウンにステートを更新する処理を追加する */}
-      
       {/* 課題4 */}
       {/* 編集を保存するボタンを押すと、todoを編集する処理を追加する */}
       {/* 課題4 */}
       {/* キャンセルするボタンを押すと、編集フォームを閉じる処理を追加する */}
-        const handleCloseEditForm = () => {
-    setIsEditable(false)
-  }
-      <div>
-        {/* 課題1 */}
-        {/* 編集用のタイトルの入力フォーム */}
-        {/* この下に書いてください */}
-        <label style={{ display: "block" }} htmlFor="title">
-          タイトル
-        </label>
-        {/* 課題1 */}
-        {/* 編集用の詳細の入力フォーム */}
-        {/* この下に書いてください */}
-        <textarea
-          style={{ width: "20em", border: "1px solid #333" }}
-          id="title"
-          name="title"
-          value={todoTitle}
-          onChange={onChangeTodoTitle}
-        />
-        {/* 課題1 */}
-        {/* 編集用のステータスのプルダウン */}
-        {/* この下に書いてください */}
-        <select value={filter}>
-          <option value="all">すべて</option>
-          <option value="notStarted">未着手</option>
-          <option value="inProgress">作業中</option>
-          <option value="done">完了</option>
-        </select>
-        {/* 課題1 */}
-        {/* 編集を保存するボタン */}
-        {/* この下に書いてください */}
-        <button>編集を保存</button>
-        {/* 課題1 */}
-        {/* キャンセルするボタン */}
-        {/* この下に書いてください */}
-        <button>キャンセル</button>
-      </div>
+      {/* 条件式 ? 真の時 : 偽の時 */}
+      {/* isEditable === true ? true : 偽の時 */}
+      {isEditable === true ? (
+        <div>
+          <label style={{ display: 'block' }} htmlFor="title">
+            タイトル
+          </label>
+          {/* 課題1 */}
+          {/* 編集用のタイトルの入力フォーム */}
+          {/* この下に書いてください */}
+          <textarea
+            style={{ width: '20em', border: '1px solid #333' }}
+            id="title"
+            name="title"
+            value={newTitle}
+            onChange={handleEditTitleChange}
+          />
+          {/* 課題1 */}
+          {/* 編集用の詳細の入力フォーム */}
+          {/* この下に書いてください */}
+          <textarea style={{ width: '20em', border: '1px solid #333' }} id="detail" name="detail" value={newDetail} />
+          {/* 課題1 */}
+          {/* 編集用のステータスのプルダウン */}
+          {/* この下に書いてください */}
+          <select>
+            <option value="all">すべて</option>
+            <option value="notStarted">未着手</option>
+            <option value="inProgress">作業中</option>
+            <option value="done">完了</option>
+          </select>
+          {/* 課題1 */}
+          {/* 編集を保存するボタン */}
+          {/* この下に書いてください */}
+          <button>編集を保存</button>
+          {/* 課題1 */}
+          {/* キャンセルするボタン */}
+          {/* この下に書いてください */}
+          <button>キャンセル</button>
+        </div>
+      ) : null}
     </>
   );
 }
